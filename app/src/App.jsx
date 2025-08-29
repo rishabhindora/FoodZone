e7a505102638f67d6ddd3da7f4b0f2232a60fd83
rishabhindora <rishabh.indora@gmail.com>
2025-08-29 22:29:43 +0530

    Add Template For Project

---
+++ app/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import styled from 'styled-components'
+
+function App() {
+    return (
+            <Container>
+                
+            </Container>)
+}
+
+var Container = styled.div`
+
+    
+
+`
+export default App
+
+
---
+++ app/src/Components/TopContainer.jsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const TopContainer = () => {
+  return (
+    <div></div>
+  )
+}
