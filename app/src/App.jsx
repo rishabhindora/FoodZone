4be8b27fd481665e6a09b5f44a41d02cbbf32cd0
rishabhindora <101365058+rishabhindora@users.noreply.github.com>
2025-08-31 02:00:38 +0530

    Add Nav and Filter (#1)
    What's Added (UI)
    
    – Contains the application logo and search bar.
    – Contains the filter buttons for refining results.

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,17 +1,18 @@
 import './App.css'
 import styled from 'styled-components'
-
+import { TopContainer } from './Components/TopContainer'
+import { FilterContainer } from './Components/FilterContainer'
 function App() {
     return (
             <Container>
-                
+                <TopContainer/>
+                <FilterContainer/>
             </Container>)
 }
 
 var Container = styled.div`
-
-    
-
+background-color: #323334 ;
+min-height: 100vh;
 `
 export default App
 
--- app/src/Components/TopContainer.jsx
+++ app/src/Components/TopContainer.jsx
@@ -1,7 +1,30 @@
 import styled from 'styled-components'
+import Logo from '../assets/images/Logo.svg'
 
 export const TopContainer = () => {
   return (
-    <div></div>
+    <Container>
+        <img src={Logo} alt="" />
+        <input type="text" placeholder='Search Food...'/>
+    </Container>
   )
 }
+
+const Container = styled.div`
+    display: flex;
+    justify-content: space-between;
+    max-width: 1200px;
+    margin: 0 auto 0 auto;
+    padding: 85px 0 54px 0;
+
+    input{
+        all: unset;
+        border-radius: 5px;
+        border: 1px solid #FF0909;
+        background: transparent;
+        height: 40px;
+        width: 285px;
+        padding-left: 15px;
+        color: white;
+    }
+`
